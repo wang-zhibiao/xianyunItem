8687c8667ce64e5a6dbc66ed41f7776796a229eb
wang-Z <wangxx599@g163.com>
2019-07-18 13:21:00 +0800

    完成机票列表部分

--- xianyun/components/air/flightsItem.vue
+++ xianyun/components/air/flightsItem.vue
@@ -45,7 +45,7 @@
             </el-col>
             <el-col :span="5" class="price">￥{{item.org_settle_price}}</el-col>
             <el-col :span="3" class="choose-button">
-              <el-button type="warning" size="mini">选定</el-button>
+              <el-button type="warning" size="mini" @click="handlechoose(data.id,item.seat_xid)">选定</el-button>
               <p>剩余：{{item.discount}}</p>
             </el-col>
           </el-row>
@@ -79,6 +79,16 @@ export default {
           const fen = min%60;
           return `${hours}时${fen}分钟`
       }  
+    },
+    methods: {
+      handlechoose(id,xid){
+        this.$router.push({
+          path:'/air/order',
+          query:{
+            id,xid
+          }
+        })
+      }
     },
   props: {
     //子组件的传值
