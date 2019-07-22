b56fbb73ce57219eccde292bb335d3d40f927b9d
张会会 <1720236230@qq.com>
2019-07-22 22:18:12 +0800

    00

--- xianyun/pages/post/detail.vue
+++ xianyun/pages/post/detail.vue
@@ -69,7 +69,29 @@
           </div>
         </el-row>
         <!-- 递归评论展示部分 -->
-        <DetailCmt :pinLunData="pinLunData"/>
+
+        <div class="cmt-list" v-for="(item,index) in pinLunData" :key="index">
+          <div class="cmt-item">
+            <div class="cmt-info">
+              <img :src="'http://157.122.54.189:9095' + item.account.defaultAvatar" />
+              <em style="font-style:normal">{{item.account.nickname}}</em>
+              <i>{{changeTime(item.created_at)}}</i>
+              <span>{{item.level}}</span>
+            </div>
+            <div class="cmt-content">
+              <p class="cmt-message">{{item.content}}</p>
+              <el-row type="flex">
+                <div class="cmt-pic" v-if="item.pics.length>0">
+                  <img :src="'http://157.122.54.189:9095' + item.pics[0].url " />
+                </div>
+              </el-row>
+              <div class="cmt-ctrl">
+                <a href="javascript:;" @click="handleDiGui(item)">回复</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <el-row type="flex" justify="center">
           <el-pagination
             @size-change="handleSizeChange"
@@ -130,7 +152,9 @@ export default {
     };
   },
   methods: {
-   
+    changeTime(created_at) {
+      return this.$moment(created_at).format("YYYY-MM-DD HH:mm");
+    },
     handleDianZan(id) {
       // 点赞功能
       this.$axios({
@@ -374,6 +398,52 @@ export default {
         position: absolute;
         left: 0;
         bottom: 0;
+      }
+    }
+  }
+}
+.cmt-list {
+  .cmt-item {
+    padding: 20px 20px 5px;
+    .cmt-info {
+      img {
+        width: 16px;
+        height: 16px;
+      }
+      span {
+        float: right;
+      }
+    }
+    .cmt-content {
+      p {
+        margin-top: 10px;
+      }
+      .cmt-pic {
+        width: 80px;
+        height: 80px;
+        border-radius: 6px;
+        border: 1px dashed #ddd;
+        padding: 5px;
+        margin-top: 10px;
+        margin-right: 5px;
+        overflow: hidden;
+        img {
+          width: 100%;
+          height: 100%;
+          cursor: pointer;
+        }
+      }
+      .cmt-ctrl {
+        height: 20px;
+        line-height: 20px;
+        text-align: right;
+        font-size: 12px;
+        color: #1e50a2;
+        a {
+          // display: none;
+          text-decoration: none;
+          color: inherit;
+        }
       }
     }
   }
